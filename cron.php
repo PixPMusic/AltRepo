f7ce5ae0a3b52ee3c7bded0bcbb5fc9d4632fbe3
PixPMusic <pixelperfectpon3@gmail.com>
2020-10-07 11:50:54 -0500

    Prep for 1.4 public, add Odyssey, official PPSSPP

--- cron.php
+++ cron.php
@@ -2,7 +2,6 @@
 include_once './repoconfig.php';
 include_once "./classes/repo.php";
 
-
 $out = fopen("repo.json", "w");
 $json = json_encode($output->getRepo());
 fwrite($out, $json);
--- repoconfig.php
+++ repoconfig.php
@@ -6,18 +6,23 @@ include_once "./classes/app/permission.php";
 include_once "./sources/unc0ver.php";
 include_once "./classes/mergerepo.php";
 include_once "./sources/externalrepo.php";
-include_once "./sources/ppsspp.php";
 
 $name = "Pixel's Repo";
-$identifier = "cc.pixp.repo";
-$sourceURL = "https://repo.potatotoast.com/repo.json";
+$identifier = "io.altstore.example";
+$sourceURL = "https://repo.pixp.cc/repo.json";
 $apps = array();
 $news = array();
+
+$odyssey = new ExternalRepo("https://theodyssey.dev/altstore/odysseysource.json", array(
+    "org.coolstar.odyssey"
+));
 
 $u0 = new unc0ver;
 MergeRepo::merge_repo($apps, $news, $u0);
 
-$ppsspp = new PPSSPP;
+$ppsspp = new ExternalRepo("https://techmunchies.net/.netlify/functions/altstore", array(
+    "org.ppsspp.ppsspp"
+));
 MergeRepo::merge_repo($apps, $news, $ppsspp);
 
 $ish = new ExternalRepo("https://ish.app/altstore.json", array(
