0b95e3c11309aed9f40ed24056591d20d7f4cd2d
PixPMusic <pixelperfectpon3@gmail.com>
2020-10-07 15:45:21 -0500

    Clean up site more

--- index.php
+++ index.php
@@ -2,26 +2,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Font Awesome -->
-    <link
-    href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-    rel="stylesheet"
-    />
-    <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
-    <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0/mdb.min.css"
-    rel="stylesheet"
-    />
-    <!-- MDB -->
-    <script
-    type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0/mdb.min.js"
-    ></script>
+    <link href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0/mdb.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0/mdb.min.js"></script>
     <title>Pixel's AltStore Repository</title>
 </head>
 <body style="background-color: #009688">
@@ -41,28 +25,20 @@
             </ul>
         </nav>
     </div>
-            <div class="container-fluid align-items-center justify-content-center h-100">
-                <div class="row d-flex justify-content-center text-center">
-                    <div class="col-md-10">
-
-                        <!-- Heading -->
-                        <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">AltStore Repo for non-Patrons</h2>
-
-                        <!-- Divider -->
-                        <hr class="hr-light">
-
-                        <!-- Description -->
-                        <h4 class="white-text my-4">Open source, even.</h4>
-                        <a type="button" class="btn btn-outline-white" href="https://github.com/PixPMusic/AltRepo">Github<i class="fa fa-book ml-2"></i></a>
-                        <a type="button" class="btn btn-outline-white" href="altstore://source?url=https://repo.pixp.cc/repo.json">Add to AltStore<i class="fa fa-book ml-2"></i></a>
-
-
-                        <br /><br />
-                        <p>This is a Proof-of-concept repository with the ability to merge several dynamic sources.</p>
-                        <p>It can act as a bridge between places like Github, or it can process other AltStore repositories, removing duplicates and ensuring the most valid source is used where possible</p>
-                        <p>It is designed to offer the fastest updates from the fastest source available within the backend repository subset.</p>
-                    </div>
-                </div>
+    <div class="container-fluid align-items-center justify-content-center h-100">
+        <div class="row d-flex justify-content-center text-center">
+            <div class="col-md-10">
+                <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">AltStore Repo for non-Patrons</h2>
+                <hr class="hr-light">
+                <h4 class="white-text my-4">Open source, even.</h4>
+                <a type="button" role="button" class="btn btn-outline-white" href="https://github.com/PixPMusic/AltRepo" style="color:white;">Github<i class="fa fa-book ml-2"></i></a>
+                <a type="button" role="button" class="btn btn-outline-white" href="altstore://source?url=https://repo.pixp.cc/repo.json" style="color:white;">Add to AltStore<i class="fa fa-book ml-2"></i></a>
+                <br /><br />
+                <p>This is a Proof-of-concept repository with the ability to merge several dynamic sources.</p>
+                <p>It can act as a bridge between places like Github, or it can process other AltStore repositories, removing duplicates and ensuring the most valid source is used where possible</p>
+                <p>It is designed to offer the fastest updates from the fastest source available within the backend repository subset.</p>
             </div>
+        </div>
+    </div>
 </body>
 
