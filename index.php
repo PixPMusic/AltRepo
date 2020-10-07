bb723ae3f189365a6384809f2eb2521cfa6fdefb
PixPMusic <pixelperfectpon3@gmail.com>
2020-10-07 15:34:16 -0500

    Clean up site

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
+<!DOCTYPE html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Font Awesome -->
     <link
     href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
@@ -22,52 +25,44 @@
     <title>Pixel's AltStore Repository</title>
 </head>
 <body style="background-color: #009688">
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar" style="-webkit-box-shadow: none!important; box-shadow: none!important">
-            <div class="container">
-                <a class="navbar-brand" href="#">Pixel's AltStore Repo</a>
-                <ul class="navbar-nav nav-flex-icons">
+    <div class="container">
+        <nav class="navbar navbar-dark" style="-webkit-box-shadow: none!important; box-shadow: none!important">
+            <a class="navbar-brand" href="#">Pixel's AltStore Repo</a>
+            <ul class="nav navbar-dark nav-flex-icons">
                 <li class="nav-item">
-                    <a class="nav-link" onclick="window.alert('PixelPerfect#0001');"><i class="fab fa-discord"></i></a>
+                    <a class="nav-link" onclick="window.alert('PixelPerfect#0001');" style="color:white;"><i class="fab fa-discord"></i></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://twitter.com/realpixp"><i class="fab fa-twitter"></i></a>
+                    <a class="nav-link" href="https://twitter.com/realpixp" style="color:white;"><i class="fab fa-twitter"></i></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://reddit.com/u/ajbiz11"><i class="fab fa-reddit"></i></a>
+                    <a class="nav-link" href="https://reddit.com/u/ajbiz11" style="color:white;"><i class="fab fa-reddit"></i></a>
                 </li>
             </ul>
-            </div>
         </nav>
-        
+    </div>
+            <div class="container-fluid align-items-center justify-content-center h-100">
+                <div class="row d-flex justify-content-center text-center">
+                    <div class="col-md-10">
 
-        <!--Mask-->
-        <div id="intro" class="view">
-            <div class="mask rgba-black-strong">
-                <div class="container-fluid d-flex align-items-center justify-content-center h-100">
-                    <div class="row d-flex justify-content-center text-center">
-                        <div class="col-md-10">
+                        <!-- Heading -->
+                        <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">AltStore Repo for non-Patrons</h2>
 
-                            <!-- Heading -->
-                            <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">AltStore Repo for non-Patrons</h2>
+                        <!-- Divider -->
+                        <hr class="hr-light">
 
-                            <!-- Divider -->
-                            <hr class="hr-light">
+                        <!-- Description -->
+                        <h4 class="white-text my-4">Open source, even.</h4>
+                        <a type="button" class="btn btn-outline-white" href="https://github.com/PixPMusic/AltRepo">Github<i class="fa fa-book ml-2"></i></a>
+                        <a type="button" class="btn btn-outline-white" href="altstore://source?url=https://repo.pixp.cc/repo.json">Add to AltStore<i class="fa fa-book ml-2"></i></a>
 
-                            <!-- Description -->
-                            <h4 class="white-text my-4">Open source, even.</h4>
-                            <button type="button" class="btn btn-outline-white" onclick="window.location.href='https://github.com/PixPMusic/AltRepo'">Github<i class="fa fa-book ml-2"></i></button>
-                            <button type="button" class="btn btn-outline-white" onclick="window.location.href='altstore://source?url=https://repo.pixp.cc/repo.json'">Add to AltStore<i class="fa fa-book ml-2"></i></button>
-                            <br /><br />
-                            <p>This is a Proof-of-concept repository with the ability to merge several dynamic sources.</p>
-                            <p>It can act as a bridge between places like Github, or it can process other AltStore repositories, removing duplicates and ensuring the most valid source is used where possible</p>
-                            <p>It is designed to offer the fastest updates from the fastest source available within the backend repository subset.</p>
-                        </div>
+
+                        <br /><br />
+                        <p>This is a Proof-of-concept repository with the ability to merge several dynamic sources.</p>
+                        <p>It can act as a bridge between places like Github, or it can process other AltStore repositories, removing duplicates and ensuring the most valid source is used where possible</p>
+                        <p>It is designed to offer the fastest updates from the fastest source available within the backend repository subset.</p>
                     </div>
                 </div>
             </div>
-        </div>
-        <!--/.Mask-->
-    </header>
 </body>
 
