39ba9bb4a3605c5d924891c8651bb06d33ab2dcc
PixPMusic <pixelperfectpon3@gmail.com>
2020-10-07 11:55:10 -0500

    Update homepage

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
     <p>This is a Proof-of-concept repository with the ability to merge several dynamic sources.</p>
     <p>It can act as a bridge between places like Github, or it can process other AltStore repositories, removing duplicates and ensuring the most valid source is used where possible</p>
     <p>It is designed to offer the fastest updates from the fastest source available within the backend repository subset.</p>
-    <p>The source is available on <a href="https://github.com/PixPMusic/AltRepo">Github</a> and the URL to add to AltRepo is <a href="https://repo.potatotoast.com/repo.json">https://repo.potatotoast.com/repo.json</a></p>
+    <p>The source is available on <a href="https://github.com/PixPMusic/AltRepo">Github</a> and the AltStore link is <a href="altstore://repo?url=https://repo.pixp.cc/repo.json">ADD TO ALTSTORE</a></p>
 </body>
