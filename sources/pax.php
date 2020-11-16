465b24da0b7d03c12bebfd4f9b42e726b8780a22
PixPMusic <pixelperfectpon3@gmail.com>
2020-11-16 11:12:01 -0600

    Add Pax Vapor

---
+++ sources/pax.php
@@ -0,0 +1,53 @@
+<?php
+include_once "./sources/source.php";
+
+class PAX extends Source {
+    private $name = "PAX Mobile";
+    private $subtitle = "Elevate your Experience";
+    private $bundleIdentifier = "com.paxlabs.paxvapor";
+    private $developerName = "PAX Labs, Inc.";
+    private $localizedDescription = "Control your PAX Device from iOS";
+    private $size = 35797093;
+    private $tintColor = "424242";
+    private $iconURL = "https://altrepo.pixp.cc/assets/pax/icon.png";
+    private $webURL = "https://www.pax.com/";
+    private $doNotify = false;
+    private $beta = false;
+    private $version = "4.0.2";
+    private $permissions = array();
+    private $definites = array("com.paxlabs.paxvapor");
+    private $versionDate = "2019-12-31";
+    private $versionDescription = "Last AppStore Release";
+    private $downloadURL = "https://altrepo.pixp.cc/assets/pax/pax.ipa";
+    private $screenshotURLs = array(
+        "https://altrepo.pixp.cc/assets/pax/screenshot.jpg"
+    );
+
+    public function getApps(): array {
+        $appitem = new AppItem(
+            $this->name, $this->bundleIdentifier, $this->developerName, $this->subtitle,
+            $this->version, $this->versionDate, $this->versionDescription, $this->downloadURL,
+            $this->localizedDescription, $this->iconURL, $this->tintColor, $this->size,
+            $this->permissions, $this->screenshotURLs, $this->beta
+        );
+        $appItems = array($appitem->getAppItem());
+        return $appItems;
+    }
+
+    public function getNews(): array {
+        $newsitem = new NewsItem(
+            $this->name . " " . $this->subtitle,
+            $this->bundleIdentifier . "-" . $this->version,
+            $this->name . " " . $this->subtitle . " out now!",
+            $this->versionDate,
+            $this->tintColor,
+            null,
+            $this->webURL,
+            $this->bundleIdentifier,
+            true
+        );
+        $newsItems = array($newsitem->getNewsItem());
+        return $newsItems;
+    }
+}
+?>
